2891481a47a422402f43ede23c222afdd151bd15
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-04-17 22:27:41 +0100

    Final

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // Clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now(); //Gives time in milliseconds
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,8 +29,31 @@ function displayTimeLeft(seconds) {
   const remainderSeconds = seconds % 60;
   const display = `${mins}:${remainderSeconds < 10
     ? '0'
-    : ''}${
-  remainderSeconds}`;
+    : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const mins = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}:${mins < 10
+    ? '0'
+    : ''}${mins}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time, 10); //always spec radix 10
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault(); // Stop GET
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
